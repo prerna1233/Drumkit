81a4d807f94ac8526f0a4a59894cacbb9bd20f54
prerna1233 <prernasai23@navgurukul.org>
2024-09-24 10:56:40 +0530

    Add files via upload

---
+++ drum.js
@@ -0,0 +1,25 @@
+let audio1 = document.getElementById('audio1');
+let audio2 = document.getElementById('audio2');
+let audio3 = document.getElementById('audio3');
+let audio4 = document.getElementById('audio4');
+let audio5 = document.getElementById('audio5');
+let audio6 = document.getElementById('audio6');
+
+function img1(){
+    audio1.play();
+}
+function img2(){
+    audio2.play();
+}
+function img3(){
+    audio3.play();
+}
+function img4(){
+    audio4.play();
+}
+function img5(){
+    audio5.play();
+}
+function img6(){
+    audio6.play();
+}
